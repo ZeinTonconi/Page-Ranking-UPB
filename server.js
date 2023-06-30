571f3bac6d67ff135f27aa531b4786b816ebc6d2
Zein Tonconi <zeintonconi@gmail.com>
2023-06-30 12:58:56 -0400

    Basic Functions

--- server.js
+++ server.js
@@ -1,28 +1,129 @@
 const axios = require("axios");
 const cheerio = require('cheerio');
 
+const initRank = 1;
+
+const G = [];
+const nodes = new Set();
+const lastRank = [];
+const rank = [];
+
+const webCrawler = async () => {
+
+    const initilPage = "https://codeforces.com";
+    const host = (new URL(initilPage)).host;
+    console.log({host});
+
+    
+
+    const queueURL = [];
+    
+    queueURL.push(initilPage);
+    nodes.add(initilPage);
+
+    // const maxPage = 50;
 
 
-const main = async () => {
-    // Link to visit
-    const htmlPage = await axios.get("https://scrapeme.live/shop/")
-    const $ = cheerio.load(htmlPage.data)    
-    // Busca todos los elementos de la clase page-numbers y que sean a
-    // Aqui se pone que elemento quiero buscar con selectores de CSS
-    $(".page-numbers a").each((index, element) => {
-        const paginationURL = $(element).attr("href");
-        //console.log(paginationURL);
-    })
-    // retrieving the product URLs 
-    $("li.product a.woocommerce-LoopProduct-link").each((index, element) => { 
-        const productURL = $(element).attr("href") 
-        //console.log(productURL)
-    })
+    while(queueURL.length !== 0 ){
+        const activeURL = queueURL.pop();
 
-     $("a").each((index, element) => { 
-        const productURL = $(element).attr("href") 
-        console.log(productURL)
-    })
+        console.log(activeURL)
+        try {
+            
+        } catch (error) {
+            
+        }
+
+        const htmlPage = await axios.get(activeURL);
+        const $ = cheerio.load(htmlPage.data);   
+        $("a").each((index,element) => {
+            
+            let neighbour;
+            neighbour = $(element).attr("href");
+            if(neighbour && neighbour.endsWith('/')){
+                neighbour = neighbour.replace(/.$/,'');
+            }
+            if(neighbour && neighbour.startsWith('/')){
+                neighbour = host + neighbour;
+            }
+            try {
+                const neighHost = (new URL(neighbour)).host;
+                if(neighHost === host){
+
+                    if(!nodes.has(neighbour) ){
+                        nodes.add(neighbour);
+                        queueURL.push(neighbour);
+                    }
+
+                    if(!G[activeURL]){
+                        G[activeURL]=new Set();
+                    }
+                    if(!G[activeURL].has(neighbour))   
+                        G[activeURL].add(neighbour);   
+                }
+            } catch (error) {
+                
+            }
+
+        }) 
+    }
 
 }
+
+const dfs = (node, visited) => {
+    
+
+    visited[node]=1;
+
+    if(!G[node])
+        return;
+    
+    G[node].forEach( neigh => {
+        if(!visited[neigh]){
+            //console.log(lastRank[node]);
+            dfs(neigh,visited);
+        }
+        rank[neigh] += lastRank[node]/G[node].size;
+    })
+}
+
+const rankPage = () => {
+
+    nodes.forEach(node => {
+        rank[node]=initRank;
+    })
+
+    const iterations = 10;
+    for(let i=0;i<iterations;i++){
+        const visited = [];
+        nodes.forEach(node => lastRank[node] = rank[node])
+        nodes.forEach(node => {
+            if(!visited[node]){
+                dfs(node,visited);
+            }
+        });
+    }
+    const ranking = [];
+    nodes.forEach(node => {
+        ranking.push({node,rank:rank[node]});
+    })
+    ranking.sort((pageA,pageB) => pageA.rank > pageB.rank? -1:0);
+    return ranking;
+}
+
+const main = async () => {
+    await webCrawler();
+    console.log("Termino el webbing");
+    const ranking = rankPage().slice(0,10);
+    console.log("RankPage")
+    console.log(ranking)
+
+    // nodes.forEach(node => {
+    //     if(G[node] && !G[node].has("https://scrapeme.live")){
+    //         console.log({node,edge:G[node]});
+    //     }
+    // })
+}
+
+
 main();
